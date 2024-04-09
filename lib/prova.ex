ed4980e005f653e6466337f4d8e9e68c8397771b
gabriel-guilherme <ubuntu@Gabriel>
2024-04-08 22:20:15 -0300

    init

---
+++ lib/prova.ex
@@ -0,0 +1,18 @@
+defmodule Prova do
+  @moduledoc """
+  Documentation for `Prova`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Prova.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
