cc1704c625ce3a59c8d8a596e008a7db50e0137b
gabriel-guilherme <ubuntu@Gabriel>
2024-04-09 21:26:44 -0300

    comments

--- lib/prova.ex
+++ lib/prova.ex
@@ -1,33 +1,61 @@
 defmodule Prova do
-  def log(x, e, k \\ 0, acc \\ 0, parte_constante \\ nil, parte_constante_quadrado \\ nil)
+  # a variavel parte_constante será o parcela do somátorio que é apenas incrementada a sua potência
+  def log(x, e, k \\ 0, acc \\ 0, parte_constante \\ nil)
 
-  def log(x, e, k, acc, nil, nil) do
+  def log(x, e, k, acc, nil) do
+    # Iniciando o codigo com precisao 100
+    Decimal.Context.set(%Decimal.Context{
+      flags: [],
+      precision: 100,
+      rounding: :half_up,
+      traps: [:invalid_operation, :division_by_zero]
+    })
+    #
+
+    # Iniciando valores que se adequem a biblioteca Decimal
     log(
       Decimal.new(x),
       Decimal.new(1, e, -35),
-      Decimal.new(k),
+      k,
       Decimal.new(acc),
-      Decimal.from_float((x - 1) / (x + 1)),
-      Decimal.mult(Decimal.from_float((x - 1) / (x + 1)), Decimal.from_float((x - 1) / (x + 1)))
+      Decimal.from_float((x - 1) / (x + 1))# x-1/x+1
     )
+    #
   end
 
-  def log(x, e, k, acc, parte_constante, parte_constante_quadrado) do
-    # 1/(2k+1)
-    parte_dependente = Decimal.div(1, Decimal.add(Decimal.mult(2, k), 1))
-    res = Decimal.mult(parte_dependente, parte_constante)
+  def log(x, e, k, acc, parte_constante) do
+    # a variável res é uma iteração particular do somatório
+    res =
+      Decimal.div(1, Decimal.add(Decimal.mult(2, k), 1)) # 1/(2k+1)
+      |> Decimal.mult(pow(parte_constante, 2 * k + 1))
 
+    # Caso de parada onde ocorre o ultimo incremento do acumulador acc, e uma vez terminado o somatório, a multiplicação por 2
     if Decimal.compare(res, e) == :lt do
       IO.puts(Decimal.mult(Decimal.add(res, acc), 2))
     else
+      # Caso recursivo onde há incremento de k + 1 para a próxima iteração do somatório, acc para acumular o valor do somátorio
+      # somando o resultado particular desta iteração
       log(
         x,
         e,
-        Decimal.add(k, 1),
+        k + 1,
         Decimal.add(acc, res),
-        Decimal.mult(parte_constante, parte_constante_quadrado),
-        parte_constante_quadrado
+        parte_constante
       )
     end
   end
+
+  #Função auxiliar para uso de potências
+  def pow(num, exp) do
+    pow(num, exp, num)
+  end
+
+  def pow(_num, 1, acc) do
+    acc
+  end
+
+  def pow(num, exp, acc) do
+    pow(num, exp - 1, Decimal.mult(num, acc))
+  end
 end
+
